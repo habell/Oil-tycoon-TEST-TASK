8655599cfdb98e8f8b1aadb3d69a5ded806a96d6
Habel <tamikys15@gmail.com>
2023-03-02 22:13:32 +0300

    Simple service locator.

---
+++ Assets/Scripts/Infrastructure/Services/AllServices.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Factory;
+using Unity.VisualScripting;
+
+namespace Infrastructure.Services
+{
+    public class AllServices
+    {
+        private static AllServices _instance;
+
+        public static AllServices Container => _instance ??= new AllServices();
+
+
+        public void RegisterSingle<TService>(TService implement) where TService : IService => 
+            Implementation<TService>.ServiceInstance = implement;
+
+        public TService Single<TService>() where TService : IService => 
+            Implementation<TService>.ServiceInstance;
+
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Services/Implementation.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure.Services
+{
+    public static class Implementation<TService> where TService : IService
+    {
+        public static TService ServiceInstance;
+    }
+}
