cf06fe27aed23fdb8934429a926300e5f7bd69da
Habel <tamikys15@gmail.com>
2023-03-02 22:51:50 +0300

    Asset provider improve.

--- Assets/Scripts/Infrastructure/AssetManagment/IAssets.cs
+++ Assets/Scripts/Infrastructure/AssetManagment/IAssets.cs
@@ -6,5 +6,6 @@ namespace Infrastructure.AssetManagment
     public interface IAssets : IService
     {
         GameObject Instantiate(string path);
+        GameObject Instantiate(GameObject guiParent, string path);
     }
 }
