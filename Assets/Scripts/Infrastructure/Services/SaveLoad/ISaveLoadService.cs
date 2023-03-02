7d309526085ef39fdbcd390ef3754759b92332bd
Habel <tamikys15@gmail.com>
2023-03-02 22:24:12 +0300

    Save\load service.

---
+++ Assets/Scripts/Infrastructure/Services/SaveLoad/ISaveLoadService.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Data;
+using Unity.VisualScripting;
+
+namespace Infrastructure.Services.SaveLoad
+{
+    public interface ISaveLoadService : ISaveService
+    { }
+
+    public interface ISaveService : ILoadService
+    {
+        void Save();
+    }
+
+    public interface ILoadService
+    {
+        void Load();
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Services/SaveLoad/SaveLoadService.cs
@@ -0,0 +1,15 @@
+﻿namespace Infrastructure.Services.SaveLoad
+{
+    public class SaveLoadService : ISaveLoadService
+    {
+        public void Load()
+        {
+            //TODO:: If i wanted to make load system, i make it here.
+        }
+
+        public void Save()
+        {
+            //TODO:: If i want to make saving system, i make it here.
+        }
+    }
+}
