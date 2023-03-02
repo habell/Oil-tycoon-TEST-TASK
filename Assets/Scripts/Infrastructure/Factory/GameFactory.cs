253ae18c3705927524924a05dfe8ca751abba0ad
Habel <tamikys15@gmail.com>
2023-03-02 22:23:08 +0300

    Simple game factory.

---
+++ Assets/Scripts/Infrastructure/Factory/GameFactory.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.AssetManagment;
+using UnityEngine;
+
+namespace Infrastructure.Factory
+{
+    public class GameFactory : IGameFactory
+    {
+        
+        
+        private readonly IAssets _assetses;
+
+        public GameFactory(IAssets assetses) => _assetses = assetses;
+
+        public GameObject CreateHUD() => _assetses.Instantiate(AssetPath.HUDPath);
+    }
+}
