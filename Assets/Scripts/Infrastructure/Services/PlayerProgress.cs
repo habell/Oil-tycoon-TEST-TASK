01ae6efa0e1b10d42519743115bb868198177423
Habel <tamikys15@gmail.com>
2023-03-03 04:53:32 +0300

    Player progress service.

---
+++ Assets/Scripts/Infrastructure/Services/PlayerProgress.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Infrastructure.Services
+{
+    public class PlayerProgress : IService
+    {
+        public List<string> playerAnswers = new List<string>(4);
+        public int playerScore;
+
+        public string GetPlayerInfo()
+        {
+            return $"Ваши баллы: {playerScore}\n\n" +
+                   $"Ваши ответы на вопросы:\n" +
+                   $"1: {playerAnswers[0]}\n" +
+                   $"2: {playerAnswers[1]}\n" +
+                   $"3: {playerAnswers[2]}\n" +
+                   $"4: {playerAnswers[3]}\n";
+        }
+    }
+}
